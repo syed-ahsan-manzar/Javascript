cd5e73ae9f2688f628c7788acaad49b32f7b8b8a
syed_ahsan_manzar <syedahsanmanzar47@gmail.com>
2022-10-31 16:14:49 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,62 @@
+// Chapter # 01(Alerts):
+alert("Syed Ahsan Manzar")
+window.alert("Welcome To Javascript")
+
+// Chapter # 02(Variables for Strings):
+var a = "Syed ";
+var b = "Ahsan ";
+var c = "Manzar. ";
+var z = a + b + c + "(Example Of Javascript Chapter # 02)";
+
+console.log(z)
+// alert(z)
+
+// Chapter # 03(Variables for Numbers):
+var x = 20;
+var y = "03";
+
+console.log(x + y)
+
+// Chapter # 04(Variable Names Legal and Illegal):
+var firstName = "Syed Ahsan Manzar ";
+var lastName = "(JS Developer)";
+var x = firstName + lastName;
+
+console.log(x)
+
+// Chapter # 05(Math expressions)
+var a = 2;
+var b = 3;
+var c = 4;
+
+var x = a + b;
+var x = a - b;
+var x = a * b;
+var x = c / a;
+var x = c % a;
+
+console.log(x)
+
+// Chapter # 06(Math expressions Icreement/Decreement)
+var x = 5;
+var y = ++x - x + --x + x - ++x + x;
+
+console.log(y)
+
+// Chapter # 07(Math expressions:Eliminating ambiguity)
+var a = 2;
+var b = 3;
+var c = 4;
+
+var x = a + b;
+var x = a + b * c;
+var x = ((a + b) * c);
+
+console.log(x)
+
+// Chapter # 08(Concatenating text strings)
+var firstName = "Java";
+var lastName = "script";
+var x = firstName + lastName;
+
+console.log(x)
